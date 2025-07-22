a978a90eead340b126457d6cec93529f32d8b50e
Bryan Gonzalez <admin@admins-MacBook-Pro.local>
2025-07-22 04:34:28 +0100

    Add SideBar component and CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
+      {/* Main content goes here */}
+      <div style={{ marginLeft: '220px', padding: '30px' }}>
+        <h1>Welcome to Game of Feeds</h1>
+        <p>This is your main content area.</p>
+      </div>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,30 @@
 import React from 'react';
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
-import logo from '../assets/logo.png'
-
+import { Navbar, Nav } from 'react-bootstrap';
+import logo from '../assets/logo.png';
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
     return (
-        <Navbar bg="light" expand="md">
-            <Container>
-                <Navbar.Brand><img src={logo} alt='logo' height='45' /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                    <Navbar.Text className="ms-auto">
-                        Signed in as: <a href="#login">Bryan Gonzalez</a>
-                    </Navbar.Text>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <div className={styles.sidebarContainer}>
+            <div className={styles.logoContainer}>
+                <img src={logo} alt="logo" className={styles.logo} />
+                <span className={styles.title}> Game of Feeds</span>
+            </div>
+
+            <div className={styles.navMiddle}>
+                <Nav className={styles.navLinks}>
+                    <Nav.Link className={styles.navLink}><i class="fa-solid fa-house"></i>  Home</Nav.Link>
+                    <Nav.Link className={styles.navLink}> <i class="fa-solid fa-arrow-right-to-bracket"></i>  Sign In</Nav.Link>
+                    <Nav.Link className={styles.navLink}> <i class="fa-solid fa-user-plus"></i>  Sign Up!</Nav.Link>
+                </Nav>
+            </div>
+
+            <div className={styles.signedIn}>
+                <Navbar.Text>
+                    Signed in as: <a href="#login">Bryan Gonzalez</a>
+                </Navbar.Text>
+            </div>
+        </div>
     );
 };
 
