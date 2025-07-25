4e940f2e2753c33ee7cb6a1c9c83ffb58df59cb0
Bryan Gonzalez <admin@MacBookPro.lan>
2025-07-25 01:08:39 +0100

    Add PostsPage component with routing and placeholders; set up CSS, assets, and filters for home, feed, and liked posts pages

--- src/App.js
+++ src/App.js
@@ -8,16 +8,23 @@ import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
 import PostCreateForm from './pages/posts/PostCreateForm';
 import PostPage from './pages/posts/PostPage';
+import PostsPage from './pages/posts/PostsPage';
+import { useCurrentUser } from './contexts/CurrentUserContext';
 
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || '';
+
   return (
     <div className={styles.App}>
       <NavBar />
       <div className={styles.ContentWrapper}>
         <Container className={styles.Main}>
           <Switch>
-            <Route exact path="/" render={() => <h1>Home Page</h1>} />
+            <Route exact path="/" render={() => <PostsPage message='No results found. Please search for another keyword.' />} />
+            <Route exact path="/feed" render={() => <PostsPage message='No results found. Start following new users or search for another keyword.' filter={`owner__followed__owner__profile=${profile_id}&`} />} />
+            <Route exact path="/liked" render={() => <PostsPage message='No results found. Start liking posts or search for another keyword.' filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`} />} />
             <Route exact path="/signup" render={() => <SignUpForm />} />
             <Route exact path="/signin" render={() => <SignInForm />} />
             <Route exact path="/posts/create" render={() => <PostCreateForm />} />
