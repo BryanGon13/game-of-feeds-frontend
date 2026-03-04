b1276759bc952fd371025eb3505dd8196da5feea
Bryan Gonzalez <bryangonzalez@MacBook-Pro.local>
2026-03-04 08:01:21 +0000

    Add REACT_APP_API_URL environment variable and update axios configuration to correctly connect frontend to backend

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://game-of-feeds-50ff6d853852.herokuapp.com/';
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.baseURL =
+  process.env.REACT_APP_API_URL || "http://localhost:8000/";
 axios.defaults.withCredentials = true;
+
+// Don't force multipart globally (set it only on upload requests)
+// axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
-
